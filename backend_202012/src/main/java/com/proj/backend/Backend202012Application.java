8ae6e3be94e45463faaa162a94605170c9ff282d
ev15963 <ev15963@hs.ac.kr>
2020-11-29 02:29:04 +0900

    backend spring setting 02:29

---
+++ backend_202012/src/main/java/com/proj/backend/Backend202012Application.java
@@ -0,0 +1,13 @@
+package com.proj.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend202012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend202012Application.class, args);
+	}
+
+}
---
+++ backend_202012/src/test/java/com/proj/backend/Backend202012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend202012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
