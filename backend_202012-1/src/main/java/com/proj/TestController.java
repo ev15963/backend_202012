700c06fd7b58fdf6f0b7ceb4fd574b98fa1e8c41
ev15963 <ev15963@hs.ac.kr>
2020-12-22 20:57:17 +0900

    backend 연동 문제 해결 중 08:57

--- backend_202012-1/src/main/java/com/proj/TestController.java
+++ backend_202012-1/src/main/java/com/proj/TestController.java
@@ -1,28 +1,45 @@
 package com.proj;
 
-import java.util.Date;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
 
-@RestController
-@RequestMapping("/api")
+//import ospringframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Controller
+//@RequestMapping("/api")
 public class TestController {
 	
-	@PostMapping(value = "/ip")
-	public ResponseEntity<String> ip (HttpServletRequest request) {
-		//요청을 보낸 클라이언트 IP 주소 반환
-		return ResponseEntity.ok(request.getRemoteAddr());
+//	@PostMapping(value = "/ip")
+//	public ResponseEntity<String> ip (HttpServletRequest request) {
+//		//요청을 보낸 클라이언트 IP 주소 반환
+//		return ResponseEntity.ok(request.getRemoteAddr());
+//	}
+	
+	@RequestMapping("/")
+	public String rootMethod() {
+		log.info("looooog");
+		
+		return "home";
 	}
 	
-	@GetMapping("/api/hello")
-	public String hello() {
-		return "안녕하세요. 현재 서버시간은 "+new Date() +"입니다. \n";
+	@GetMapping(value="/hello")
+	public String hello(Model model) {
+		log.info("helloooo");
+//		UserVO user = new UserVO();
+//		user.setUserID("아이디");
+//		user.setName("이름");
+		model.addAttribute("name", "userdddd");
+		
+		return "hello";
 	}
 }
