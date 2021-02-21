5a5f3e0e447806e7f9e4c4df621ff07c7b56d6d1
ev15963 <ev15963@hs.ac.kr>
2021-02-21 23:46:48 +0900

    단위태스트 mock객체 23:46

---
+++ backend_202012-1/src/test/java/com/proj/backend/TestControllerTest.java
@@ -0,0 +1,14 @@
+package com.proj.backend;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
